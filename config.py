f364e0ba422b64f6502d89187c06311880177fff
omaloshtanova <omaloshtanova@yandex.ru>
2020-05-12 15:07:23 +0700

    Fix

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 DB_FILE = 'db/db.sqlite'
+UPLOAD_DIR = 'static/uploads'
 
 AUTHORIZED_REDIRECT = '/'
 UNAUTHORIZED_REDIRECT = '/login'
--- main.py
+++ main.py
@@ -2,30 +2,25 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads
 
-from config import DB_FILE
+from config import *
 from data import db_session
 from routes import public_bp, admin_bp, route_utils
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'srr61dSLjxW/BRSMmwoTcGDWiaCqcqGXp8BfdQ7Y+Uo='
+app.config['UPLOADED_FILES_DEST'] = UPLOAD_DIR
+app.register_blueprint(public_bp)
+app.register_blueprint(admin_bp)
 
-def main():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'srr61dSLjxW/BRSMmwoTcGDWiaCqcqGXp8BfdQ7Y+Uo='
-    app.config['UPLOADED_FILES_DEST'] = 'static/uploads'
-    app.register_blueprint(public_bp)
-    app.register_blueprint(admin_bp)
+db_session.global_init(DB_FILE)
 
-    db_session.global_init(DB_FILE)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.user_loader(route_utils.user_loader)
+login_manager.unauthorized_handler(route_utils.unauthorized)
 
-    login_manager = LoginManager()
-    login_manager.init_app(app)
-    login_manager.user_loader(route_utils.user_loader)
-    login_manager.unauthorized_handler(route_utils.unauthorized)
-
-    app.uploader = UploadSet()
-    configure_uploads(app, app.uploader)
-
-    app.run()
-
+app.uploader = UploadSet()
+configure_uploads(app, app.uploader)
 
 if __name__ == '__main__':
-    main()
+    app.run()
